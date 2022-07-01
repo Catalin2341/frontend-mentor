7f3c14412fc315a1e878fdacbf45f95044ced993
Catalin2341 <79571811+Catalin2341@users.noreply.github.com>
2022-07-01 13:23:47 +0300

    solved

--- index.js
+++ index.js
@@ -15,38 +15,41 @@ let pers = 0;
 let sum = 0;
 
 function getSum(ev) {
-  sum = document.getElementById(ev).value;
+  sum = parseFloat(document.getElementById(ev).value);
   checkout();
-  //   console.log("SUM: ", sum);
 }
 
 function getPers(ev) {
-  pers = document.getElementById(ev).value;
+  pers = parseInt(document.getElementById(ev).value);
   checkout();
   //   console.log("PERS: ", pers);
 }
 
 function checkout() {
   if (sum && pers && parseInt(last.split("%")[0])) {
-    console.log("Tip ammount", sum / parseInt(last.split("%")[0]) / pers);
-    console.log(
-      "Total ammount",
-      (sum / parseInt(last.split("%")[0]) / pers) * pers
-    );
+    let proc = parseInt(last.split("%")[0]);
+    proc = proc / 100;
+    let tip = formatToFixed(sum * proc);
+    let total_tip = formatToFixed(parseFloat(tip / pers));
+    let total_pers = formatToFixed(parseFloat((sum + tip) / pers));
+    document.getElementById("total_tip").innerText = "$" + total_tip;
+    document.getElementById("total_sum").innerText = "$" + total_pers;
   }
 }
 
-function test() {
-  let sum2 = 142.55,
-    proc2 = 15,
-    pers2 = 5;
-  console.log("TIP:", (100 * proc2) / sum2);
-  console.log("Tip ammount", sum2 / proc2 / pers2);
-  console.log("Total ammount", (sum2 / proc2 / pers2) * pers2);
+function formatToFixed(par) {
+  // console.log("NUM:", num);
+  let num = par.toString();
+  if (num.includes(".")) {
+    let ans = "";
+    num = num.split(".");
+    ans += num[0] + "." + num[1].slice(0, 2);
+    ans = parseFloat(ans);
+    return ans;
+  }
+  num = parseFloat(num);
+  return num;
 }
-test();
-test();
-test();
 
 function setStyle() {
   for (let i in inpt) {
@@ -59,5 +62,8 @@ function selectedTip(ev) {
   inpt[last] = false;
   inpt[ev.value] = true;
   last = ev.value;
+  if (sum && pers) {
+    checkout();
+  }
   setStyle();
 }
